587a3bfc0cbf77d6530c17ba187cafda8abe5445
Andrei Shablyka <kloppp0007@gmail.com>
2019-10-11 05:52:38 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstArr = first.split('').reverse();
+  let secondArr = second.split('').reverse();
+  let resultArr = [];
+
+  for (let i = 0; i < secondArr.length; i++) {
+    for (let j = 0; j < firstArr.length; j++) {
+      let adder = firstArr[j] * secondArr[i];
+      if (resultArr[i + j]) {
+        resultArr[i + j] += adder;
+      } else resultArr[i + j] = adder;
+    }
+  }
+  
+  for (let i = 0; i < resultArr.length; i++) {
+    let memory = Math.floor(resultArr[i] / 10);
+    resultArr[i] = resultArr[i] % 10;
+    if (memory) {
+      if (resultArr[i + 1]) {
+      resultArr[i + 1] += memory;
+      } else resultArr[i + 1] = memory;
+    }
+  }
+
+  return resultArr.reverse().join('');
 }
